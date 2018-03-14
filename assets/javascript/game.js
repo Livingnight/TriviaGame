5e09572eba38f5ed87da01fe400f302dc9468528
unknown <jjernigan1065@gmail.com>
2018-03-14 14:29:02 -0400

    making functions to call into game function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,6 +26,12 @@ $(document).ready(function(){
     function reset(){
         $(".mainBody").append(gameObj.heading, "<br>", gameObj.button);
     }
+    function stop(){
+
+    }
+    function pageClear(){
+        $(".mainBody").empty();
+    }
     function countDown(){
 
         gameObj.time--;
@@ -39,9 +45,8 @@ $(document).ready(function(){
 
 
             console.log("this is an alert");
-            $(".mainBody").empty();
-
-            $(".mainBody").append(gameObj.heading, "<br>");
+            pageClear();
+            reset();
 
             // $(".mainBody").append()
 
@@ -49,7 +54,7 @@ $(document).ready(function(){
             console.log(" this is the length of answers: "+gameObj.questions[gameObj.count].answers.length);
             console.log("this is the answers[0]: " + gameObj.questions[gameObj.count].answers[0]);
             for(var i = 0; i < gameObj.questions[gameObj.count].answers.length; i++){
-                $("<div>").append(gameObj.questions[gameObj.count].answers[i]);
+                $("div").append($("<div>").text(gameObj.questions[gameObj.count].answers[i]));
             }
 
 
