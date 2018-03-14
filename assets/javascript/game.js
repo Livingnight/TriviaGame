c974496e5254576353e5883523db168f3f1cc816
unknown <jjernigan1065@gmail.com>
2018-03-14 14:34:12 -0400

    finished putting pieces into functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,9 +26,18 @@ $(document).ready(function(){
     function reset(){
         $(".mainBody").append(gameObj.heading, "<br>", gameObj.button);
     }
-    function stop(){
-
+    function setQuestion(){
+        $(".mainBody").append($("<div>").text(gameObj.questions[gameObj.count].question));
     }
+
+    function setAnswers(){
+        for(var i = 0; i < gameObj.questions[gameObj.count].answers.length; i++){
+            $("div").append($("<div>").text(gameObj.questions[gameObj.count].answers[i]));
+        }
+    }
+    // function stop(){
+    //
+    // }
     function pageClear(){
         $(".mainBody").empty();
     }
@@ -50,12 +59,10 @@ $(document).ready(function(){
 
             // $(".mainBody").append()
 
-            $(".mainBody").append($("<div>").text(gameObj.questions[gameObj.count].question));
+            setQuestion();
             console.log(" this is the length of answers: "+gameObj.questions[gameObj.count].answers.length);
             console.log("this is the answers[0]: " + gameObj.questions[gameObj.count].answers[0]);
-            for(var i = 0; i < gameObj.questions[gameObj.count].answers.length; i++){
-                $("div").append($("<div>").text(gameObj.questions[gameObj.count].answers[i]));
-            }
+            setAnswers();
 
 
         })
